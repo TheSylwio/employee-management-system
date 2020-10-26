78ea4d7132cc746bc4f8e5f36404a29b79b9e43e
thesylwio <thesylwio@gmail.com>
2020-10-26 19:40:43 +0100

    🧹 Reformat

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,13 +16,11 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('index');
-         }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('index');
+        }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
