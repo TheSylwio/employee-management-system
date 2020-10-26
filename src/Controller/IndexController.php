fb161a8679bc628b8f76ab4b568960d5be00c179
thesylwio <thesylwio@gmail.com>
2020-10-26 17:21:43 +0100

    🎨 Navbar layout improvement

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,8 +12,6 @@ class IndexController extends AbstractController
      */
     public function index()
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->render('index/index.html.twig');
     }
 }
