81e51cfd0545931f35fd7295a8c1e8f2b7ef5514
Daniel <dandys9987@gmail.com>
2020-10-30 18:55:18 +0100

    dev/addEvent created form,adding event to database

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Form\EventType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventsController extends AbstractController
@@ -17,6 +20,28 @@ class EventsController extends AbstractController
 
         return $this->render('events/index.html.twig', [
             'events' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/events/add", name="eventsadd")
+     * @param Request $request
+     * @return Response
+     */
+    public function add(Request $request)
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+            $this->addFlash('success', 'wydarzenie dodane');
+            return $this->redirectToRoute('events');
+        }
+        return $this->render('events/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
