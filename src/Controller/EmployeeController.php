78144b8c0e7493b09ccf2f59ea03828b4a0b7a07
anton002 <antoni.gebauer@edu.uekat.pl>
2020-10-30 18:49:46 +0100

    Created form, added 3 sample employees to employee entity.

---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employee;
+use App\Form\EmployeeType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmployeeController extends AbstractController
+{
+    /**
+     * @Route("/employee", name="employee")
+     * @param $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $employee = new Employee();
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($employee);
+            $em->flush();
+        }
+        return $this->render('employee/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
