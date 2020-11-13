389275175da2b3c55f54cc1b68227b8f71828f04
thesylwio <thesylwio@gmail.com>
2020-11-13 14:29:40 +0100

    🧹 Cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,44 @@
+// FIXME: Refactor
 const firstButton = document.querySelector("#today");
 const secondButton = document.querySelector("#sevenDays");
-const thirdButton=document.querySelector("#all");
+const thirdButton = document.querySelector("#all");
 const today = document.getElementsByName("today");
 const others = document.getElementsByName("others");
 const sevenDays = document.getElementsByName("sevenDays");
 const elements = document.getElementsByClassName("eventRow");
+
 if (firstButton) {
-    firstButton.addEventListener("click", () => {
-        for (let i = 0; i < today.length; i++) {
-            today[i].style.visibility = 'visible';
-        }
-        for (let i = 0; i < sevenDays.length; i++) {
-            sevenDays[i].style.visibility = 'collapse';
-        }
-        for (let i = 0; i < others.length; i++) {
-            others[i].style.visibility = 'collapse';
-        }
-    })
+  firstButton.addEventListener("click", () => {
+    for (let i = 0; i < today.length; i++) {
+      today[i].style.visibility = 'visible';
+    }
+    for (let i = 0; i < sevenDays.length; i++) {
+      sevenDays[i].style.visibility = 'collapse';
+    }
+    for (let i = 0; i < others.length; i++) {
+      others[i].style.visibility = 'collapse';
+    }
+  })
 }
+
 if (secondButton) {
-   secondButton.addEventListener("click", () => {
-       for (let i = 0; i < today.length; i++) {
-           today[i].style.visibility = 'visible';
-       }
-       for (let i = 0; i < sevenDays.length; i++) {
-           sevenDays[i].style.visibility = 'visible';
-       }
-       for (let i = 0; i < others.length; i++) {
-           others[i].style.visibility = 'collapse';
-       }
-    })
+  secondButton.addEventListener("click", () => {
+    for (let i = 0; i < today.length; i++) {
+      today[i].style.visibility = 'visible';
+    }
+    for (let i = 0; i < sevenDays.length; i++) {
+      sevenDays[i].style.visibility = 'visible';
+    }
+    for (let i = 0; i < others.length; i++) {
+      others[i].style.visibility = 'collapse';
+    }
+  })
 }
 
 if (thirdButton) {
-    thirdButton.addEventListener("click", () => {
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].style.visibility = 'visible';
-        }
-    })
+  thirdButton.addEventListener("click", () => {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.visibility = 'visible';
+    }
+  })
 }
