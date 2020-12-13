26729114cdb2dc35def121eb0163802ac396d196
Daniel <dandys9987@gmail.com>
2020-12-13 12:58:03 +0100

    layout changes

--- assets/js/slidebar.js
+++ assets/js/slidebar.js
@@ -1,5 +1,6 @@
 $(document).ready(function (){
     $("#sidebarCollapse").on('click',function (){
         $("#sidebar").toggleClass('active')
+        $(this).find($(".fas")).toggleClass('fa-arrow-down').toggleClass('fa-arrow-up')
     })
 })
