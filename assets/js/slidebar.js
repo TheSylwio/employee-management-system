6f165f3acf6719d1571b5428b8053de3534ee976
Daniel <dandys9987@gmail.com>
2020-12-15 11:14:22 +0100

    changed css to scss, classes rename

--- assets/js/slidebar.js
+++ assets/js/slidebar.js
@@ -1,6 +1,5 @@
 $(document).ready(function (){
     $(".dropdown").on('click',function (){
-        console.log("JD");
         $(this).toggleClass('active')
         $(this).find($(".fas")).toggleClass('fa-arrow-down').toggleClass('fa-arrow-up')
     })
