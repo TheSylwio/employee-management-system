4cd42b9afd6f7c979b0d6b68cdd5860519c448ef
thesylwio <thesylwio@gmail.com>
2020-12-20 17:11:57 +0100

    ✨ Modified template

--- src/Form/DocumentType.php
+++ src/Form/DocumentType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
 
 class DocumentType extends AbstractType
 {
@@ -20,18 +19,7 @@ class DocumentType extends AbstractType
             ->add('filename', FileType::class, [
                 'label' => 'Plik dokumentu',
                 'mapped' => false,
-                'constraints' => [
-                    new File([
-                        'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'application/pdf',
-                            'application/x-pdf',
-                        ],
-                        'mimeTypesMessage' => 'Please upload a valid PDF document',
-                    ])
-                ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
