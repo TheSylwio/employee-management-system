116fbf65ed3a0d07edf2755b59296c3eb127b1d0
thesylwio <thesylwio@gmail.com>
2020-12-20 16:12:10 +0100

    ✨ Implemented uploading documents

--- src/Controller/DocumentsController.php
+++ src/Controller/DocumentsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Document;
+use App\Form\DocumentType;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +14,31 @@ class DocumentsController extends AbstractController
 {
     /**
      * @Route("/documents", name="documents")
+     * @param Request $request
+     * @param FileUploader $fileUploader
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, FileUploader $fileUploader): Response
     {
+        $document = new Document();
+        $form = $this->createForm(DocumentType::class, $document);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $documentFile = $form->get('filename')->getData();
+            $documentFileName = $fileUploader->upload($documentFile);
+            $document->setFilename($documentFileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($document);
+            $em->flush();
+
+            $this->addFlash('success', 'Pomyślnie dodano dokument!');
+            return $this->redirectToRoute('documents');
+        }
+
         return $this->render('documents/index.html.twig', [
-            'controller_name' => 'DocumentsController',
+            'form' => $form->createView(),
         ]);
     }
 }
