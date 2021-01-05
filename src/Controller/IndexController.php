c92c6b45f46e5deac6cb7d49203c0945f8ccb7bc
thesylwio <thesylwio@gmail.com>
2021-01-05 15:59:19 +0100

    ✨ Implementing post deleting

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostType;
 use App\Service\Helper;
+use mysql_xdevapi\Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -52,5 +54,24 @@ class IndexController extends AbstractController
         return $this->render('index/add_post.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/post/{post}/delete", name="post_delete", methods={"POST"})
+     * @param Post $post
+     * @return Response
+     */
+    public function delete(Post $post): Response
+    {
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($post);
+            $em->flush();
+        } catch (Exception $exception) {
+            return new JsonResponse($exception->getMessage(), 500);
+        }
+
+        $this->addFlash('success', 'Pomyślnie usunięto post');
+        return new JsonResponse('Pomyślnie usunięto post', 200);
     }
 }
