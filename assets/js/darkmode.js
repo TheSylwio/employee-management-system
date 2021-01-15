fc720ab14c0f615e71420eac853b76923a33e3ed
thesylwio <thesylwio@gmail.com>
2021-01-15 13:42:33 +0100

    ✨ Logo is changing color now!

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,4 +1,11 @@
-const btn = document.querySelector(".btn-toggle");
-btn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+const btn = document.querySelector("#btn-toggle");
+let switched = false;
+
+btn.addEventListener("click", () => {
+  const logo = document.querySelector('#dark-logo');
+
+  logo.src = switched ? 'https://127.0.0.1:8000/images/logo.svg' : 'https://127.0.0.1:8000/images/logo_white.svg';
+  switched = !switched;
+
+  document.body.classList.toggle("dark-mode");
 });
