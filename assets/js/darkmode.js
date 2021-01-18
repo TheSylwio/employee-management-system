87fda524ce053fe6d0503554becbce63e554b3b9
thesylwio <thesylwio@gmail.com>
2021-01-18 20:01:27 +0100

    🔥 Removed blue version of logo

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -4,12 +4,9 @@ const currentTheme = localStorage.getItem("mode");
 
 if (currentTheme === "dark") {
   document.body.classList.add("dark-mode");
-  logo.src = 'https://127.0.0.1:8000/images/logo_white.svg';
 }
 
 btn.addEventListener("click", () => {
-  logo.src = (currentTheme === 'dark') ? 'https://127.0.0.1:8000/images/logo.svg' : 'https://127.0.0.1:8000/images/logo_white.svg';
-
   document.body.classList.toggle("dark-mode");
   
   const theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
